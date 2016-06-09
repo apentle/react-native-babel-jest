80e3c235dea6af3fc1f2019ab7c3ff77d3e5104f
Doan Nguyen <doannd@apentle.com>
2016-06-09 16:54:47 +0700

    Simple preset react-native & CI test example

--- index.js
+++ index.js
@@ -5,9 +5,7 @@
 'use strict';
 
 const babel = require('babel-core');
-const jestPreset = require('babel-preset-jest');
-const es2015Preset = require('babel-preset-es2015');
-const reactPreset = require('babel-preset-react');
+const reactNative = require('babel-preset-react-native');
 
 module.exports = {
   process(src, filename) {
@@ -15,7 +13,7 @@ module.exports = {
       return babel.transform(src, {
         auxiliaryCommentBefore: ' istanbul ignore next ',
         filename,
-        presets: [jestPreset, es2015Preset, reactPreset],
+        presets: [reactNative],
         retainLines: true,
       }).code;
     }
