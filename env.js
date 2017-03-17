d46aa239067552c8ad6fc7c431859b932f3ce5fe
Doan Nguyen <doannd@apentle.com>
2017-03-17 17:23:05 +0700

    Fix for both react-native 0.42 and before

--- env.js
+++ env.js
@@ -1,6 +1,11 @@
 'use strict';
 
-require.requireActual('react-native/packager/src/Resolver/polyfills/babelHelpers.js');
+var fs = require('fs');
+if (fs.existsSync('./node_modules/react-native/packager/src/Resolver/polyfills/babelHelpers.js')) {
+  require.requireActual('react-native/packager/src/Resolver/polyfills/babelHelpers.js');
+} else {
+  require.requireActual('react-native/packager/react-packager/src/Resolver/polyfills/babelHelpers.js');
+}
 
 global.__DEV__ = true;
 global.__fbBatchedBridgeConfig = {
@@ -14,4 +19,4 @@ global.regeneratorRuntime = require.requireActual('regenerator-runtime/runtime')
 /**
  * Mock react-native
  */
-jest.setMock('react-native', require('./react-native'));
+jest.setMock('react-native', require('./react-native/index'));
